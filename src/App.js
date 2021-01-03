0144f9bb96c85c3b0a17ea3cd43cb4e9c6d6ecfb
Alan Elrefai <alaaelrefai88@gmail.com>
2021-01-03 17:59:53 -0300

    finishing

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import Footer from "./components/footer";
 function App() {
   return (
     <div className="App">
-      <Footer />
+      <Footer img='https://lojaanimale.vteximg.com.br/arquivos/ids/1245100-658-987/25052614_6218_1-SHORT-DE-COURO-COS-BOTOES.jpg?v=637413283474770000'
+      price='100,00' discountedPrice='90,00' name='RAGLAN FIO TINTO LISTRA CAROL'/>
     </div>
   );
 }
--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,19 +1,37 @@
+import React, {useState} from 'react';
 import './style.css';
 
 function Footer(props) {
+    const [loading, setLoading] = useState(false);
+
+    // simulando o carregamento de botão
+    function loadButton() {
+        setLoading(true)
+        setTimeout(()=>{
+            setLoading(false);
+        },2000)
+    }
     return(
         <div  className="footer">
             <div className="wrapper">
                 <div className="img-footer">
-                    <img src={props.img} alt={'error'}/>
+                    <img src={props.img} alt='product'/>
                     <div className="info">
-                        <div>RAGLAN FIO TINTO LISTRA CAROL</div>
-                        <p>Preço: R$  {props.price} </p>
-                        <p>Preço com desconto: R$ {props.price}</p>    
+                        <div>{props.name}</div>
+                        <p>Preço: R${props.price} </p>
+                        <p>Preço com desconto: R${props.discountedPrice}</p>
                     </div>
                 </div>
+                {}
                 <div className='buttons'>
-                    <button className='buy-btn'>ADICIONAR A SACOLA</button>
+                    {loading ?
+                    (<button className='buy-btn spinner'>
+                        <div className="bounce1"></div>
+                        <div className="bounce2"></div>
+                        <div className="bounce3"></div>
+                    </button>):
+                    (<button className='buy-btn' onClick={loadButton}> ADICIONAR A SACOLA</button>)
+                    }
                     <button className='close-btn'><span>x</span></button>
                 </div>
             </div>
